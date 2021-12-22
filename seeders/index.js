a64b495f05b91a183a9f7c84902b159048ca460d
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2021-12-22 11:50:33 -0800

    back to register

--- seeders/index.js
+++ seeders/index.js
@@ -9,9 +9,9 @@ async function seeder() {
   console.log('----Seeding Data----')
 
   try {
-    await User.create(new User({ username: 'johndoe', email: 'johndoe@gmail.com' }), 'password1234')
-    await User.create(new User({ username: 'janedoe', email: 'janedoe@gmail.com' }), 'password4321')
-    await User.create(new User({ username: 'jackdoe', email: 'jackdoe@gmail.com' }), 'rootroot')
+    await User.register(new User({ username: 'johndoe', email: 'johndoe@gmail.com' }), 'password1234')
+    await User.register(new User({ username: 'janedoe', email: 'janedoe@gmail.com' }), 'password4321')
+    await User.register(new User({ username: 'jackdoe', email: 'jackdoe@gmail.com' }), 'rootroot')
     // await Post.bulkCreate(require('./postSeed.js'))
   } catch (err) {
     console.log(err)
