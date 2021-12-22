6dda941892c4a54e10e4cafb4f768c0c48088116
Loganuwu <53189601+Loganuwu@users.noreply.github.com>
2021-12-22 11:44:47 -0800

    uncommenting to get user create working

--- seeders/index.js
+++ seeders/index.js
@@ -1,26 +1,26 @@
-// require('dotenv').config()
+require('dotenv').config()
 
-// const { User, Post, Comment } = require('../models')
-// const sequelize = require('../db')
+const { User, Post, Comment } = require('../models')
+const sequelize = require('../db')
 
-// async function seeder() {
-//   await sequelize.sync({ force: true })
+async function seeder() {
+  await sequelize.sync({ force: true })
 
-//   console.log('----Seeding Data----')
+  console.log('----Seeding Data----')
 
-//   try {
-//     await User.register(new User({ username: 'johndoe', email: 'johndoe@gmail.com' }), 'password1234')
-//     await User.register(new User({ username: 'janedoe', email: 'janedoe@gmail.com' }), 'password4321')
-//     await User.register(new User({ username: 'jackdoe', email: 'jackdoe@gmail.com' }), 'rootroot')
-//     await Post.bulkCreate(require('./postSeed.js'))
-//   } catch (err) {
-//     console.log(err)
-//   }
+  try {
+    await User.create(new User({ username: 'johndoe', email: 'johndoe@gmail.com' }), 'password1234')
+    await User.create(new User({ username: 'janedoe', email: 'janedoe@gmail.com' }), 'password4321')
+    await User.create(new User({ username: 'jackdoe', email: 'jackdoe@gmail.com' }), 'rootroot')
+    // await Post.bulkCreate(require('./postSeed.js'))
+  } catch (err) {
+    console.log(err)
+  }
 
 
-//   console.log('----Data Seeded----')
+  console.log('----Data Seeded----')
 
-//   process.exit()
-// }
+  process.exit()
+}
 
-// seeder()
+seeder()
