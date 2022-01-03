1e41b465b57dd7474f42134ec6ce780b702abe88
Kevin K <kelleyindahouse@gmail.com>
2022-01-03 01:56:26 -0500

    confirmed routes functionality and added css file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { User, Post, Comment } = require('../models')
+const { User, Post } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
