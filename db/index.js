8403d3d6535d40598ef1e621355892192b309200
wearehavingfun <tim.bassignana@gmail.com>
2022-01-05 12:10:43 -0800

    production toggle

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/blog_db')
+module.exports = new Sequelize(process.env.NODE_ENV !== 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/blog_db')
 
 // const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/learnearn_db')
 
