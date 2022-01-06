9dc4be483a8476c9834f917cde8dae35b11210a5
Kevin K <kelleyindahouse@gmail.com>
2022-01-05 20:08:33 -0500

    changed index to match with deploy branch

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.NODE_ENV !== 'production' ? process.env.JAWSDB_URI : 'mysql://root:rootroot@localhost:3306/blog_db')
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
+
 
 // const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/learnearn_db')
 
