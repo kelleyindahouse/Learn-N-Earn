20f9da3c831c2ac27bc55c3f5b8f4effb596b134
Kevin K <kelleyindahouse@gmail.com>
2022-01-06 02:15:15 -0500

    changes

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize')
 
-// module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
+module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
 
-// module.exports = new Sequelize(process.env.NODE_ENV !== 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/learnnearn_db')
+// module.exports = new Sequelize(process.env.NODE_ENV !== 'production' ? process.env.JAWSDB_URL : 'mysql://root:rootroot@localhost:3306/learnearn_db')
 
-const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/learnearn_db')
+// const sequelize = new Sequelize('mysql://root:rootroot@localhost:3306/learnearn_db')
 
-module.exports = sequelize
+// module.exports = sequelize
 
